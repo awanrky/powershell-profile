e83ee02ea96aaf9c166292a6bc76b44fb2e123b2
Mark Ott <markgilbertott@gmail.com>
2013-05-15 18:55:48 -0500

    added profile script for the ISE

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -1,5 +1,8 @@
 $ProfileDir = (split-path $MyInvocation.MyCommand.Path -Parent)
 
 Push-Location $ProfileDir
-	. ./powershell-profile.ps1
+	. ./prompt.ps1
+	. ./eye-candy.ps1
+	. ./aliases.ps1
+	. ./local-network-script.ps1
 Pop-Location
