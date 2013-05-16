405225a8aacb50b4da8e6d5e61ece23ebae13da8
Mark Ott <markgilbertott@gmail.com>
2013-05-15 19:18:32 -0500

    added path to visual studio

--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -1,8 +1,11 @@
 $ProfileDir = (split-path $MyInvocation.MyCommand.Path -Parent)
 
 Push-Location $ProfileDir
+	. ./miscellaneous-functions.ps1
 	. ./prompt.ps1
 	. ./eye-candy.ps1
 	. ./aliases.ps1
 	. ./local-network-script.ps1
 Pop-Location
+
+Add-ToPath "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE"
